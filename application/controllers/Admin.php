264f944cf81f291ba591c203d1994d2690cedfac
abhi2707 <abhisheka@247around.com>
2020-08-16 22:16:17 +0530

    Render function

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -31,6 +31,28 @@ class Admin extends CI_Controller
 	}
 	
 	
+		/**
+	 * @param string     $view
+	 * @param array|null $data
+	 * @param bool       $returnhtml
+	 *
+	 * @return mixed
+	 */
+	public function _render_page($view, $data = NULL, $returnhtml = FALSE)//I think this makes more sense
+	{
+
+		$viewdata = (empty($data)) ? $this->data : $data;
+
+		$view_html = $this->load->view($view, $viewdata, $returnhtml);
+
+		// This will return html on 3rd argument being true
+		if ($returnhtml)
+		{
+			return $view_html;
+		}
+	}
+	
+	
 	 
 
 }

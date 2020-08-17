3ad111ab917fcb894b627c3d5285eaf368184995
abhi2707 <abhisheka@247around.com>
2020-08-17 23:02:21 +0530

    Validation fix

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,13 +1,4 @@
 
-<script src="<?php echo base_url(); ?>assets/bundles/libscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js --> 
-<script src="<?php echo base_url(); ?>assets/bundles/vendorscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js --> 
-<script src="<?php echo base_url(); ?>assets/plugins/jquery-validation/jquery.validate.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/jquery-steps/jquery.steps.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/select2/select2.min.js"></script> <!-- Select2 Js -->
-
-<script src="<?php echo base_url(); ?>assets/plugins/bootstrap-notify/bootstrap-notify.js"></script> 
-<script src="<?php echo base_url(); ?>assets/bundles/mainscripts.bundle.js"></script><!-- Custom Js --> 
-<script src="<?php echo base_url(); ?>assets/js/pages/forms/advanced-form-elements.js"></script> 
 
 </body>
 </html>
